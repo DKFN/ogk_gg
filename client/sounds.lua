b3f524bff4ca40882f9357ecd56dd6d696f5776f
DoctorWhoFR <doctorwhofr@protonmail.com>
2019-12-24 17:47:22 +0100

    Maybe fix sound volume

--- client/sounds.lua
+++ client/sounds.lua
@@ -1,6 +1,7 @@
 
 AddRemoteEvent("PlayLevelUp", function()
     local sound = CreateSound("sounds/smb/smb_powerup.wav")
+    SetSoundVolume(sound, 0.3)
     if sound == false then
         AddPlayerChat("Error playing sound")
     end    
@@ -8,6 +9,7 @@ end)
 
 AddRemoteEvent("PlayPickupSound", function()
     local sound = CreateSound("sounds/smb/smb_1-up.wav")
+    SetSoundVolume(sound, 0.3)
     if sound == false then
         AddPlayerChat("Error playing sound")
     end    
@@ -15,14 +17,17 @@ end)
 
 AddRemoteEvent("PlayerIsWinner", function()
     CreateSound("sounds/smb/smb_stage_clear.wav")
+    SetSoundVolume(sound, 0.3)
 end)
 
 AddRemoteEvent("PlayerIsLooser", function()
     CreateSound("sounds/smb/smb_gameover.wav")
+    SetSoundVolume(sound, 0.3)
 end)
 
 AddRemoteEvent("GameRestarting", function()
     CreateSound("sounds/quake/prepare.wav")
+    SetSoundVolume(sound, 0.3)
 end)
 
 local hsInstance
@@ -31,4 +36,5 @@ AddRemoteEvent("TrueDmgHeadShot", function()
         DestroySound(hsInstance)
     end 
     hsInstance = CreateSound("sounds/quake/headshot.wav")
+    SetSoundVolume(hsInstance, 0.3)
 end)
